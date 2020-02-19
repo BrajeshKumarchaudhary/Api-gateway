2ef3a4ada866e72c7463b577b25406c2a0a9b584
brajesh <brajesh.kumar@squareyards.co.in>
2020-02-19 10:07:34 +0530

    code changes

--- login/src/main/java/com/login/login/LoginApplication.java
+++ login/src/main/java/com/login/login/LoginApplication.java
@@ -2,8 +2,10 @@ package com.login.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.login")
 public class LoginApplication {
 
 	public static void main(String[] args) {
